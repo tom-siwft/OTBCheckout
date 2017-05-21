d88a5d13ac9cee5d7727784c4ab8d09f7c2f0388
tom-swift <tom@tintinet.co.uk>
2017-05-21 10:56:53 +0100

    Added Failing Tests for B,C and D

--- CheckoutKata/CheckoutKata.Tests/ValuesReturned.cs
+++ CheckoutKata/CheckoutKata.Tests/ValuesReturned.cs
@@ -12,5 +12,26 @@ namespace CheckoutKata.Tests
             int value = checkout.GetTotal();
             Assert.AreEqual(value, 50);
         }
+        [TestMethod]
+        public void B_Returns_Correct_Single_Value()
+        {
+            var checkout = new Checkout();
+            int value = checkout.GetTotal();
+            Assert.AreEqual(value, 30);
+        }
+        [TestMethod]
+        public void C_Returns_Correct_Single_Value()
+        {
+            var checkout = new Checkout();
+            int value = checkout.GetTotal();
+            Assert.AreEqual(value, 20);
+        }
+        [TestMethod]
+        public void D_Returns_Correct_Single_Value()
+        {
+            var checkout = new Checkout();
+            int value = checkout.GetTotal();
+            Assert.AreEqual(value, 15);
+        }
     }
 }
