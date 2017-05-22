641c6e41054839fc20f08846a9e1a352317202b4
tom-swift <tom@tintinet.co.uk>
2017-05-22 18:49:49 +0100

    Added basic Failing Test for A offer

---
+++ CheckoutKata/CheckoutKata.Tests/OffersCalculated.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Tests
+{
+    [TestClass]
+    public class OffersCalculated
+    {
+        [TestMethod]
+        public void A_Returns_Correct_Offer_Value()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            int value = checkout.GetTotal();
+            Assert.AreEqual(value, 130);
+        }
+    }
+}
