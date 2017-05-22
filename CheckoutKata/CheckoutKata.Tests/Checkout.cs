68621c3d0708b4a1d7b34c912c27c0dcc91ba449
tom-swift <tom@tintinet.co.uk>
2017-05-22 20:02:05 +0100

    Made offer A Test Pass

--- CheckoutKata/CheckoutKata.Tests/Checkout.cs
+++ CheckoutKata/CheckoutKata.Tests/Checkout.cs
@@ -17,10 +17,48 @@ namespace CheckoutKata.Tests
         public int GetTotal()
         {
             int total = 0;
-            foreach (var sku in _skus)
+            //order and group skus
+            var groupedSKUList = _skus.GroupBy(s => s);
+
+            foreach (var group in groupedSKUList)
             {
-                var item = _data.items.FirstOrDefault(i => i.SKU == sku);
-                total += item.UnitPrice;
+                var sku = group.Key.ToString().ToUpper();
+                //get sku quantity
+                var quantity = group.Count();
+                //if offer is triggered calculate remainder
+                int SpecialCondition = Convert.ToInt32(_data.items.Where(c => c.SKU == sku).Select(c => c.SpecialCondition ?? 0).First());
+                if (SpecialCondition > 0)
+                {
+                    int timesToRunOffer = quantity / SpecialCondition;
+                    int remainder;
+                    int q = Math.DivRem(quantity, SpecialCondition, out remainder);
+
+                    if (timesToRunOffer > 0)
+                    {
+                        var offerPrice = _data.items.Where(x => x.SKU == sku && x.SpecialCondition == quantity).Select(x => x.SpecialPrice ?? 0).First();
+                        for (int i = 0; i < timesToRunOffer; i++)
+                        {
+                            total += offerPrice;
+                        }
+                    }
+
+                    if (remainder > 0)
+                    {
+                        var item = _data.items.FirstOrDefault(i => i.SKU == group.Key.ToString());
+                        for (int i = 0; i < remainder; i++)
+                        {
+                            total += item.UnitPrice;
+                        }
+                    }
+                }
+                else
+                {
+                    var item = _data.items.FirstOrDefault(i => i.SKU == group.Key.ToString());
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        total += item.UnitPrice;
+                    }
+                }
             }
             return total;
         }
